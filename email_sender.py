841abf6d51b6171fd8cb7e5649f82879527db7ea
Ashok Nath <nathashok87@gmail.com>
2020-08-14 14:46:19 +1000

    Change email again to gmail

--- email_sender.py
+++ email_sender.py
@@ -1,19 +1,23 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Python Email'
 email['to'] = 'nathashok87@gmail.com'
 email['subject'] = 'You won a million dollar from a broke ass lottery ticket'
 
-email.set_content(
-    'I am Python devloper. \nLearning to script from this language')
+email.set_content(html.substitute({'name': 'Tintin'}), 'html')
 
-with smtplib.SMTP(host='smtp.yahoo.com', port=465) as smtp:
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('emailpython@yahoo.com', 'Send@message1')
+    smtp.login('emailpython39@gmail.com', 'SendEmail1')
     smtp.send_message(email)
     print('all good boss email sent!')
+
 
 # Send@message1
